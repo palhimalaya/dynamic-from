7fc009b9ee41ec6b18723370a0a7e23a97584703
palhimalaya <himalayapal123@gmail.com>
2026-05-06 18:39:41 +0545

    remove unnecessary codebase including Prisma setup, controllers, routes, and types

--- src/routes/analytics.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-import { getSurveyAnalytics } from "../controllers/analytics.controller.js";
-
-const router = Router();
-
-router.get("/:id/analytics", getSurveyAnalytics);
-
-export default router;
--- src/routes/response.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-import { submitResponse } from "../controllers/response.controller.js";
-
-const router = Router();
-
-router.post("/:id/responses", submitResponse);
-
-export default router;
